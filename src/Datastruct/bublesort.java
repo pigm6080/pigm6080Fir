9291e4f6494910c4c34f306c6a405c0f899da1fe
pigm6080 <pigm6080@gmail.com>
2021-12-24 23:29:51 +0900

    buble

---
+++ src/Datastruct/bublesort.java
@@ -0,0 +1,21 @@
+package Datastruct;
+
+import java.util.Arrays;
+
+public class bublesort {
+	void bublesort(int []arr) {
+		int temp = 0;
+		for(int i=0;i<arr.length;i++) {
+			for(int j = 1; i <arr.length-i;j++) {
+				if(arr[j-1] > arr[j]) {
+					temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
+}
